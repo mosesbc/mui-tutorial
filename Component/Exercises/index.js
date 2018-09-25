fa5a1f39b4fd63368357e7075e329586b1817a28
mosesbc <mosescalma@outlook.ph>
2018-09-26 00:00:23 +0800

    Initial commit

---
+++ Component/Exercises/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => {
+  return (
+    <div className="App">
+      <h1>Hello Moses</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+};
--- README.md
+++
@@ -1,2 +0,0 @@
-# mui-tutorial
-Created with CodeSandbox
